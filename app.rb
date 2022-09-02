ad169ea9fc5484d48d30218c9106b36355ba9e1d
nikos <nikos@localhost.localdomain>
2022-09-02 20:00:56 +0300

    создание таблицы posts  в базе данных blog.db

--- app.rb
+++ app.rb
@@ -10,6 +10,15 @@ def init_db
 before do
 	init_db
 end
+configure do
+	init_db
+@db.execute 'CREATE TABLE IF NOT EXISTS "POSTS" (
+	"id"	INTEGER,
+	"created_date"	DATE,
+	"content"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)';
+	end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
