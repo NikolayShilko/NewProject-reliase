765a8f62d8b0b8bbef13513abe4be8c6e1e8f848
nikos <nikos@localhost.localdomain>
2022-09-02 20:48:07 +0300

    добавлены комментарии к коду и обработчик ошибок

--- app.rb
+++ app.rb
@@ -3,15 +3,21 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+#создание базы данных Blog.db    
+
 def init_db
 	@db=SQLite3::Database.new 'Blog.db'
 	@db.results_as_hash= true
 	end
+#метод Before вызывается каждый раз при перезагрузке страницы
+# служит для обновления данных в бд.
 before do
 	init_db
 end
+
 configure do
-	init_db
+	init_db #инициализация БД
+	#создание таблицы POSTS если не существует
 @db.execute 'CREATE TABLE IF NOT EXISTS "POSTS" (
 	"id"	INTEGER,
 	"created_date"	DATE,
@@ -19,13 +25,23 @@ configure do
 	PRIMARY KEY("id" AUTOINCREMENT)
 )';
 	end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
 get '/Post' do
   erb :new
 end
+ 
+ #обработчик Post запроса /new
 post '/new' do
+# получение данных из формы в new.erb и запись  в переменную content	
   content= params[:content]
-  "Вы ввели:#{content}"
+#проверка параметров данных введенных в форму
+if content.length ==0  
+	@error="Введите текст!"
+	return erb :new
+end
+  erb "Вы ввели:#{content}"
 end
